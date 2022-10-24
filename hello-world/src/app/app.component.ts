45d3efd3ec85153fc4f5f618438cec3f949f2016
nevosmic <nevosmic@post.bgu.ac.il>
2022-10-24 12:35:19 +0300

    basic table sample data

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { TableService } from './table.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface UserData {
+  name: string;
+  average: string;
+  total_time: string;
+  email_name: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'hello-world';
+  displayedColumn: string[] = ['name', 'average', 'total_time', 'email_name'];
+  // "non-null assertion operator", is a way to tell typescript that you know for sure the value of a property is neither null nor undefined.
+  dataSource!: MatTableDataSource<UserData>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator; // child of dataSource
+  @ViewChild(MatSort) sort!: MatSort; // child of dataSource
+
+  posts: any; // results comong from the API
+
+  constructor(private service: TableService) {
+    // get data coming from the table service into the console
+    // getData returns an observable
+    this.service.getData().subscribe((data) => {
+      console.log(data);
+      data = [
+        {
+          name: 'Michal',
+          average: '75 %',
+          total_time: '2733',
+          email_name: 'nevosmic28',
+        },
+        {
+          name: 'Yael',
+          average: '100 %',
+          total_time: '3157',
+          email_name: 'Jablonka',
+        },
+      ];
+      this.posts = data;
+      this.dataSource = new MatTableDataSource(this.posts);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    // remain something after filter
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
